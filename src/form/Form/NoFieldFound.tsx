b8284e5d8d3b4cdc26555d44be2fcf90096e2ca1
Rage Lopez <VrtxOmega@pm.me>
2026-06-11 09:09:49 -0500

    Fix no-field message on All tab

--- src/form/Form/NoFieldFound.tsx
+++ src/form/Form/NoFieldFound.tsx
@@ -4,22 +4,32 @@ import { CanvasFormTabsContext } from '../providers/canvas-form-tabs.provider';
 
 export const NoFieldFound: FunctionComponent<{ className?: string }> = (props) => {
   const canvasFormTabsContext = useContext(CanvasFormTabsContext);
+  const isAllTabSelected = canvasFormTabsContext.selectedTab === 'All';
 
   return (
     <Card data-testid="no-field-found" className={props.className}>
       <CardBody>
-        <Alert variant="info" title={`No ${canvasFormTabsContext.selectedTab} fields found`}>
-          No field found matching this criteria. Please switch to the{' '}
-          <Button
-            onClick={() => {
-              canvasFormTabsContext.setSelectedTab('All');
-            }}
-            variant="link"
-            isInline
-          >
-            <b>All</b>
-          </Button>{' '}
-          tab.
+        <Alert
+          variant="info"
+          title={isAllTabSelected ? 'No fields found' : `No ${canvasFormTabsContext.selectedTab} fields found`}
+        >
+          No field found matching this criteria.
+          {!isAllTabSelected && (
+            <>
+              {' '}
+              Please switch to the{' '}
+              <Button
+                onClick={() => {
+                  canvasFormTabsContext.setSelectedTab('All');
+                }}
+                variant="link"
+                isInline
+              >
+                <b>All</b>
+              </Button>{' '}
+              tab.
+            </>
+          )}
         </Alert>
       </CardBody>
     </Card>
